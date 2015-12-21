1e12bb5cc04c2e5b9ec89550bb001db7a9e8e53b
Keno Dressel <ardobras@gmail.com>
2015-12-22 00:29:35 +0100

    first tries with node.js, for more ask @ardobras

--- gruntfile.js
+++ gruntfile.js
@@ -3,55 +3,96 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     files: {
-      js: ['gruntfile.js', 'js/**/*.js', 'server/**/*.js'],
+      js: ['public/js/**/*.js', '*.js']
     },
     sass: {
       dist: {
         options: {
-          sourcemap: 'none'
+          sourceMap: false
         },
         files: {
-          'css/styles.css': 'sass/styles.scss'
+          'public/css/styles.css': 'sass/styles.scss'
         }
       }
     },
     jshint: {
       files: '<%= files.js %>',
       options: {
-        jshintrc: true,
-      },
+        jshintrc: true
+      }
     },
     jsbeautifier: {
-      files: '<%= files.js %>',
+      files: ['public/js/**/*.js', '*.js', 'sass/**/*.scss'],
       options: {
         js: {
           indentChar: ' ',
-          indentSize: 2,
+          indentSize: 2
         },
         css: {
           fileTypes: ['.scss'],
           indentChar: ' ',
           indentSize: 2,
+          wrapLineLength: 100
+        }
+      }
+    },
+    uglify: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: 'public/js',
+          src: ['*.js', '!*.min.js'],
+          dest: 'public/js',
+          ext: '.min.js'
+        }]
+      }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'public/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'public/css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    eol: { // Task
+      target: {
+        options: {
+          eol: 'lf',
+          replace: true
+        },
+        files: {
+          src: [
+            'public/*',
+            'public/css/*',
+            'public/js/*'
+          ]
         }
       }
     },
     watch: {
       css: {
         files: ['sass/**/*.scss'],
-        tasks: ['sass'],
+        tasks: ['sass']
       },
       js: {
         files: '<%= files.js %>',
-        tasks: ['jshint', 'jsbeautifier'],
+        tasks: ['jsbeautifier', 'jshint']
       }
     }
   });
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-jsbeautifier');
-  //Specify an output folder for minified files
-  //grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-eol');
 
-  grunt.registerTask('default', ['jshint', 'jsbeautifier', 'sass']);
+  grunt.registerTask('default', ['jsbeautifier', 'jshint', 'sass', 'uglify', 'cssmin',
+    'eol'
+  ]);
 };
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 'use strict';
 var express = require('express');
+var request = require('request');
+var handlebars = require('handlebars');
+
 var app = express();
 
-app.use(express.static(__dirname + '/../'));
+app.use(express.static(__dirname + '/public/'));
 
-var server = app.listen(3000, function() {
+var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
