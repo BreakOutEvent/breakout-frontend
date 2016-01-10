d7f658ea8c7efd59bb6420cdd805d63405822488
Keno <ardobras@googlemail.com>
2016-01-10 15:48:33 +0100

    extracts the data and render layer

---
+++ controller/dataProxy.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Keno on 10.01.2016.
+ */
+'use strict';
+var fs = require('fs');
+
+//Define empty object
+var dataLayer = {};
+
+dataLayer.checkForRenderedFiles = function(folder,id) {
+
+  return fs.readdirSync(__dirname + '/rendered/' + folder).filter(function(filename) {
+      return filename.match(new RegExp(id + '-' + '\d*.html')) !== null;
+    }) || [];
+
+};
+
+dataLayer.readRenderedFile = function(folder,id) {
+
+  return fs.readFileSync(__dirname + '/rendered/' + folder + '/' + id + '.html');
+
+};
+
+dataLayer.readTemplateFile = function(folder,id) {
+
+  return fs.readFileSync(__dirname + '/templates/' + folder + '/' + id + '.hbs');
+
+};
+
+module.exports = dataLayer;
--- controller/templateProxy.js
+++ controller/templateProxy.js
@@ -1,12 +1,9 @@
 'use strict';
 var mongoose = require('./mongo.js');
-var handlebars = require('handlebars');
 var fs = require('fs');
+var renderer = require('./renderProxy.js');
+var data = require('./dataProxy.js');
 
-//Define Models
-var Page = mongoose.model('page', require('../schemas/page.js'));
-var View = mongoose.model('view', require('../schemas/view.js'));
-var Template = mongoose.model('template', require('../schemas/template.js'));
 
 //Define empty object
 var proxy = {};
@@ -17,15 +14,18 @@ var proxy = {};
 
 proxy.init = function () {
 
-  Page.find({}, function (err, docs) {
+  //Define Models
+  var Page = mongoose.model('page', require('../schemas/page.js'));
+
+  Page.find({}, function (err, pages) {
     if (err) {
       console.log(err);
     } else {
-      docs.forEach(function (doc) {
-        proxy.getPage(doc._id, function () {
+      pages.forEach(function (page) {
+        proxy.getPage(page._id, function () {
           //Do we need to do sth?
         });
-      })
+      });
     }
   });
 
@@ -36,26 +36,19 @@ proxy.init = function () {
 
 proxy.getPage = function (pageID, cb) {
 
-  var pages = checkForFiles('page', pageID);
+  var pages = data.checkForRenderedFiles('page', pageID);
 
   if(pages.length > 0) {
     //Found some pages with the correct IDs
     pages.sort();
 
-    fs.readFile(__dirname + '/rendered/page/' + pages[0], function (err, data) {
-      if (err) {
-        throw err;
-      }
-      console.log(data);
-    });
-
+    cb(data.readRenderedFile('page',pages[0]));
 
   } else {
     //Nothing found just render it
-    proxy.renderPage(pageID, cb);
+    renderer.renderPage(pageID, cb);
 
   }
-
 
   //1. Check if there is one or more HTML files for this page
   //1.1 If YES, send the most current version -- done
@@ -63,92 +56,24 @@ proxy.getPage = function (pageID, cb) {
 
 };
 
-proxy.renderPage = function (pageID, cb) {
 
-  var tempViewHTML = [];
-
-  Page.findOne({'_id': pageID}, function (err, page) {
-    if (err) {
-      throw err;
-    } else {
-
-      //Render HTML for each View
-      page.views.forEach(function (view) {
-        proxy.getView(view._id, function (html) {
-          tempViewHTML.push({'_id': view._id, 'html': html});
-        });
-      });
-
-      //Fill template with HTML from Views
-      //For static pages we assume that we just concat the views
-      var html = tempViewHTML.map(function (el) {
-        return el.html;
-      }).join("");
-
-      //Read page template
-      var handlebarsTemplate = readHandlebarsTemplateSync('page', pageID);
-
-      //Compile template to function
-      var compiledTemplate = handlebars.compile(handlebarsTemplate);
-
-      //Callback with completed html
-      cb(compiledTemplate({'body': html}));
-    }
-  });
-
-  //1. Grab all views for this page
-  //2. For each view, execute this.renderView
-  //3. wait for the callback
-  //4. Render the page by combining the view HTML files
-  //5. Save the rendered page as HTML with timestamp
-
-};
 
 proxy.getView = function (viewID, cb) {
 
-  var views = checkForFiles('view', viewID);
+  var views = data.checkForRenderedFiles('view', viewID);
 
   if(views.length > 0) {
     //Found some pages with the correct IDs
     views.sort();
 
-    fs.readFile(__dirname + '/rendered/page/' + views[0], function (err, data) {
-      if (err) {
-        throw err;
-      }
-      console.log(data);
-    });
+    cb(data.readRenderedFile('view',views[0]));
 
 
   } else {
     //Nothing found just render it
-    proxy.renderView(viewID, cb);
+    renderer.renderView(viewID, cb);
 
   }
-
-};
-
-proxy.renderView = function (viewID, cb) {
-
-  View.findOne({'_id': viewID}, function (err, view) {
-    if (err) {
-      throw err;
-    } else {
-      //Read page template
-      var handlebarsTemplate = readHandlebarsTemplateSync('view', viewID);
-
-      //Compile template to function
-      var compiledTemplate = handlebars.compile(handlebarsTemplate);
-
-      //Callback with completed html
-      cb(compiledTemplate(view.values));
-    }
-  });
-
-  //1. Grab the template for this view
-  //2. Combine the template with the vars from the view
-  //3. Render the view and save as atomic HTML file with timestamp
-
 };
 
 proxy.checkForViewUpdates = function () {
@@ -170,18 +95,5 @@ proxy.checkForViewUpdates = function () {
    */
 
 };
-
-
-function readHandlebarsTemplateSync(folder, id) {
-
-}
-
-function checkForFiles(folder,id) {
-
-  return fs.readdirSync(__dirname + '/rendered/' + folder).filter(function(filename) {
-    return filename.match(new RegExp(id + '-' + '\d*.html')) !== null;
-  }) || [];
-
-}
 
 module.exports = proxy;
