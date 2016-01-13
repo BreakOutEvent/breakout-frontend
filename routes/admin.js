2343c93ba43f651577e8c5a845acc6ba09e43491
Markus Heimbuchner <mheimbuc@hm.edu>
2016-01-13 20:32:38 +0100

    Add dummy user database Add all dependencies required by login Login mostly done; login/logout work
    Signed-off-by: Markus Heimbuchner <mheimbuc@hm.edu>

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,16 @@
 
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('admin/home',
         {
             layout: 'admin',
-            path: '/admin'
+            path: '/admin',
+            success: req.flash('success'),
+            user: req.user
         }
     );
 });
@@ -21,7 +24,8 @@ router.get('/about', function (req, res, next) {
     res.render('admin/about',
         {
             layout: 'admin',
-            path: '/admin'
+            path: '/admin',
+            user: req.user
         }
     );
 });
@@ -30,9 +34,42 @@ router.get('/contact', function (req, res, next) {
     res.render('admin/contact',
         {
             layout: 'admin',
-            path: '/admin'
+            path: '/admin',
+            user: req.user
         }
     );
 });
+
+router.get('/login', function (req, res, next) {
+    res.render('admin/login',
+        {
+            layout: 'admin',
+            path: '/admin',
+            error: req.flash('error')
+        }
+    );
+});
+
+router.post('/login',
+    passport.authenticate('local',
+        {
+            failureRedirect: '/admin/login',
+            //failureFlash: "Login not successful",
+            successRedirect: '/admin',
+            //successFlash: "Successfully logged in!"
+            failureFlash: true,
+            successFlash: true
+        }
+    )
+);
+
+
+router.get('/logout',
+    function (req, res) {
+        req.logout();
+        req.flash('success', 'Successfully logged out!');
+        res.redirect('/admin/');
+    }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,53 @@ var express = require('express');
 var request = require('request');
 var exphbs = require('express-handlebars');
 var path = require('path');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var db = require('./schemas/user');
+
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
+passport.use(new Strategy(
+    function (username, password, cb) {
+        db.findByUsername(username, function (err, user) {
+            if (err) {
+                return cb(err);
+            }
+            if (!user) {
+                return cb(null, false, {message: 'User does not exist!'});
+            }
+            if (user.password != password) {
+                return cb(null, false, {message: 'Username/password do not match'});
+            }
+            return cb(null, user, {message: 'Successfully logged in'});
+        });
+    }));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+    db.findById(id, function (err, user) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, user);
+    })
+});
+
 
 var app = express();
 var hbs = exphbs.create({
@@ -12,14 +59,24 @@ var hbs = exphbs.create({
     }
 });
 
-//hbs.registerHelper("equal", );
-
 // handlebars is now default engine
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('express-session')({secret: 'keyboard cat', resave: false, saveUninitialized: false}));
+app.use(require('body-parser').urlencoded({extended: true}));
+app.use(require('cookie-parser')());
+app.use(require('connect-flash')());
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Sets routes
 app.use('/', require('./routes/main'));
