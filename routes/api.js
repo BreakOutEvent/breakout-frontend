bc321da9e9e34d41d1c232210249b4bfcbda50e8
Isigiel <lu.heddendorp@gmail.com>
2016-03-13 14:40:47 +0100

    Set cors headers for development

--- routes/api.js
+++ routes/api.js
@@ -15,6 +15,11 @@ var express = require('express');
 var reader = require('../controller/templateReader.js');
 var router = express.Router();
 
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 router.get('/getList', function(req, res) {
   reader.getAll(function(templates) {
