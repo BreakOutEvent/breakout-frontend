1b577b0a7d03f7cc92a657f3e9dc0ff4e244f011
Lukas Heddendorp <l.heddendorp@celonis.de>
2016-03-16 17:13:44 +0100

    Change logging on the page editor

--- public/cms/src/app/siteEditor/siteEditor.js
+++ public/cms/src/app/siteEditor/siteEditor.js
@@ -21,27 +21,24 @@ class pageEditorCtrl {
     this._page = Page
     this._template = Template
     this._log = $log
-    this._log.debug('Editor initilized, current page:')
-    this._log.debug(this.page)
+    this._debug('Editor initilized, current page:', this.page)
     this._http.get('//localhost:3000/api/css').then((res) => {
       this.style = this._sce.trustAsCss(res.data)
-      this._log.debug('## CSS loaded ### styles get scoped ##')
+      this._debug('## CSS loaded ### styles get scoped ##')
       scoper.scopeStyles()
       scoper.process(res.data)
     })
     $rootScope.$watch(()=>{return this.page}, () => {
-      this._log.debug('Page changed')
+      this._debug('Page changed, current page: ', this.page)
       this.reload()
     })
   }
   reload () {
-    this._log.debug('Reloading page contents')
-    this._log.debug(this.page.views)
+    this._debug('Reloading page contents, current views: ', this.page.views)
     this.views = []
     this.page.views.forEach((view, index) => {
       this._view.get({view_id: view}).$promise.then((viewRes) => {
-        this._log.debug('###ViewResponse###')
-        this._log.debug(viewRes)
+        this._debug('View call finished, response: ', viewRes)
         this._template.getHtml(viewRes.templateName).then((res) => {
           this.views[index] = {
             view: viewRes,
@@ -51,16 +48,22 @@ class pageEditorCtrl {
       })
     })
   }
+  private _debug (msg, dump) {
+    let text = "boEditor - "+msg
+    this._log.debug(text)
+    if(dump) {
+      this._log.debug(dump)
+      this._log.debug('----DUMP COMPLETE----')
+    }
+  }
   drop (event, index, item) {
     let vm = this
     var view = new this._view
-    this._log.debug('###Dropped Item###')
-    this._log.debug(item)
+    this._debug('Item was dropped, content:', item)
     view.templateName = item.name
     view.variables = item.vars
     view.$save(null, function (call) {
-      vm._log.debug('###Save callback###')
-      vm._log.debug(call)
+      vm._debug('view.$save got called with data:', call)
       vm.page.views.splice(index, 0, call._id)
       vm._page.update({pageId: vm.page._id}, vm.page)
       vm.reload()
