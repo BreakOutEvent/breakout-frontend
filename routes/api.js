51546126e5b6bb5f68164f39a0507512093c59ea
Keno Dressel <ardobras@gmail.com>
2016-03-19 20:02:01 +0100

    adds a proper POST /view endpoint to get initialized templates. also fixes some major inconsistencies.

--- routes/api.js
+++ routes/api.js
@@ -28,29 +28,6 @@ router.get('/getList', function(req, res) {
   });
 });
 
-router.post('/batch/:model', function(req, res) {
-
-  var model = models[req.params.model] ? models[req.params.model] : null;
-
-  if (!model) {
-    res.sendStatus(404);
-    return;
-  }
-
-  if (!req.body) {
-    res.sendStatus(400);
-    return;
-  }
-
-  models[req.params.model].find({ _id : { $in : req.body } }).exec(function (err, docs) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(docs);
-    }
-  });
-});
-
 router.get('/css', function (req, res) {
   fs.readFile('public/css/temp_style.min.css', function(err, file) {
     if(err) {
@@ -85,6 +62,75 @@ router.get('/html/:name', function (req, res) {
       res.send(file);
     });
   }
+});
+
+
+router.post('/batch/:model', function(req, res) {
+
+  var model = models[req.params.model] ? models[req.params.model] : null;
+
+  if (!model) {
+    res.sendStatus(404);
+    return;
+  }
+
+  if (!req.body) {
+    res.sendStatus(400);
+    return;
+  }
+
+  models[req.params.model].find({ _id : { $in : req.body } }).exec(function (err, docs) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(docs);
+    }
+  });
+});
+
+//OVERWRITE
+router.post('/view', function(req, res) {
+
+  if (!req.body || !req.body.name) {
+    res.sendStatus(400);
+    return;
+  }
+
+  reader.getByName(req.body.name, function(template) {
+    if(!template) {
+      res.sendStatus(404);
+    }
+
+    console.log(template);
+
+    var rawView = {};
+    rawView.templateName = template.name;
+    rawView.variables = [];
+
+    //FILL WITH DEFAULT VALUES
+    template.variables.forEach(function(variable) {
+      variable.values = [
+        {language:'de', value:'defaultValue'},
+        {language:'en', value:'defaultValue'}
+      ];
+      rawView.variables.push(variable);
+    });
+
+    console.log(rawView);
+
+    models["view"].create(rawView, function (err, docs) {
+      if (err) {
+        console.log(req.body);
+        res.send(err);
+      } else {
+        res.json(docs);
+      }
+    });
+
+  });
+
+
+
 });
 
 
