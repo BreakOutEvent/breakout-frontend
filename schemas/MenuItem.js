1b2978bbb6dad8f2accc2e0ef1c752b107132e2d
Keno <ardobras@googlemail.com>
2016-03-23 12:48:17 +0100

    rename MenuItem to menuItem

--- schemas/MenuItem.js
+++ schemas/MenuItem.js
@@ -2,14 +2,11 @@
 var mongoose = require('mongoose');
 
 var itemSchema = new mongoose.Schema({
-  properties: [{
-    language: {type: mongoose.Schema.Types.Lang, required: true},
-    title: {type: String, required: true},
-    _page: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'page'
-    }
-  }]
+  title: {type: String, required: true},
+  _page: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'page'
+  }
 });
 
 module.exports = itemSchema;
