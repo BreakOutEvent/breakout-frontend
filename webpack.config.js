e771ad94bb334915d69b9327389f78fad9de9fa2
TomBrand <thommy1988@gmail.com>
2016-04-08 23:14:26 +0200

    replaced browserify with webpack; vertical alignment changes
    - browserify removed
    - added webpack (WIP)
    - added babel for frontend js
    - fixed some vertical alignment issues

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+'use strict';
+var webpack = require('webpack'),
+    path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/js/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'public/js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { 
+        test: /\.js$/, 
+        include: [
+          path.resolve(__dirname, 'src/js')
+        ],
+        exclude: /node_modules/, 
+        loader: 'babel-loader' 
+      }
+    ]
+  }
+};
