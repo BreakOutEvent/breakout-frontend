68326e252d347a489c2e6d47f40c6e9a22b2eb8a
Lukas Heddendorp <lu.heddendorp@gmail.com>
2016-04-26 14:27:43 +0200

    Add authorization to allowed cors headers ... again

--- routes/api.js
+++ routes/api.js
@@ -38,7 +38,7 @@ router.use((req, res, next) => {
   res.set({
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   });
 
   if ('OPTIONS' == req.method) {
