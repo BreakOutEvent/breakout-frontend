61204e06481409a8470f06ba614666d6a00594cc
Keno <ardobras@googlemail.com>
2016-05-11 09:06:41 +0200

    fixes grunt error

--- src/js/sponsoring.js
+++ src/js/sponsoring.js
@@ -6,6 +6,23 @@ $(document).ready(() => {
   const text = $('#amountPerKm_text');
   const limit = $('#limit');
 
+  function updateOutput() {
+    let output = `Ein Team hat 2015 im Durchschnitt 800km zurück gelegt. Bei ${text.val()}€
+      pro Kilometer ergäbe das eine Spendensummme von ${Math.round(800 * text.val())}€.`;
+    if (limit.val() > 0 && limit.val() < Math.round(800 * text.val())) {
+      output += ` Durch das Limit wird die Spendensumme
+      auf maximal ${Math.round(limit.val())}€ beschränkt.`;
+    }
+    $('#output').html(output);
+    $('#estimate').html(() => {
+      if(limit.val() > 0 && limit.val() < Math.round(800 * text.val())) {
+        return Math.round(limit.val());
+      } else {
+        return Math.round(800 * text.val());
+      }
+    });
+  }
+
   updateOutput();
 
   range.on('input', () => {
@@ -22,20 +39,6 @@ $(document).ready(() => {
     updateOutput();
   });
 
-  function updateOutput() {
-    let output = `Ein Team hat 2015 im Durchschnitt 800km zurück gelegt. Bei ${text.val()}€
-      pro Kilometer ergäbe das eine Spendensummme von ${Math.round(800 * text.val())}€.`;
-    if (limit.val() > 0 && limit.val() < Math.round(800 * text.val())) {
-      output += ` Durch das Limit wird die Spendensumme auf maximal ${Math.round(limit.val())}€ beschränkt.`;
-    }
-    $('#output').html(output);
-    $('#estimate').html(() => {
-      if(limit.val() > 0 && limit.val() < Math.round(800 * text.val())) {
-        return Math.round(limit.val());
-      } else {
-        return Math.round(800 * text.val());
-      }
-    })
-  }
+
 
 });
