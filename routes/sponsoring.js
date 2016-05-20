b5ea1739871bd3698f5eccf085dee7c42ceccbe5
Keno Dressel <ardobras@googlemail.com>
2016-05-20 16:14:58 +0200

    removes unneeded comments from sponsoring route

--- routes/sponsoring.js
+++ routes/sponsoring.js
@@ -15,7 +15,6 @@ const upload = multer({inMemory: true});
 const router = express.Router();
 
 router.get('/sponsoring', session.isUser, (req, res, next) => co(function*() {
-//router.get('/sponsoring', (req, res, next) => co(function*() {
 
 
   //CHECK IF USER IS SPONSOR OR PARTICIPANT
@@ -23,25 +22,7 @@ router.get('/sponsoring', session.isUser, (req, res, next) => co(function*() {
     req.flash(`error`, `Um diese Seite aufzurufen, musst Du entweder Teil eines Teams oder ein Sponsor sein.`);
     return res.redirect('/selection');
   }
-/*
-  let me = {
-    firstname:"Henrike",
-    lastname:"von Zimmermannn",
-    gender:"male",
-    participant: {
-      teamId: 10,
-      eventId: 1,
-      teamName: "Awesome",
-      eventCity: "München"
-    },
-    email: "test@test.de",
-    sponsor: {
-      id: 10
-    }
-  };
 
-  req.user = {};
-  req.user.me = me;*/
   let incSponsoring = [];
   let incChallenges = [];
   let outSponsoring = [];
@@ -53,7 +34,6 @@ router.get('/sponsoring', session.isUser, (req, res, next) => co(function*() {
   }
 
 
-  ///console.log(incSponsoring);
 
   //OUTGOING
   if(req.user.status.is.sponsor) {
@@ -63,15 +43,6 @@ router.get('/sponsoring', session.isUser, (req, res, next) => co(function*() {
 
 
   const teams = yield sponsoring.getAllTeams(req);
-
-  /*const teams = [{
-    id: 1,
-    name: "TEST",
-    eventCity: "München",
-    event: 1
-  }];*/
-
-  console.log(outSponsoring);
 
   res.render(`dynamic/sponsoring/sponsoring`,
     {
@@ -93,22 +64,14 @@ router.get('/sponsoring', session.isUser, (req, res, next) => co(function*() {
 //SPONSRING ROUTES
 
 router.post('/sponsoring/create', session.hasTeam, upload.single('contract'), sponsoring.create);
-router.put('/sponsoring/edit', session.isSponsor, upload.single('contract'), (req, res, next) => co(function*() {
-  console.log(req.body);
-  res.send(200);
-}).catch(ex => next(ex)));
-
-
 router.post('/sponsoring/accept', session.isUser, sponsoring.accept);
 router.post('/sponsoring/reject', session.isUser, sponsoring.reject);
-//TODO wait for backend
-//router.post('/sponsoring/delete', session.isUser, sponsoring.delete);
+router.post('/sponsoring/delete', session.isUser, sponsoring.delete);
 
 //CHALLENGE ROUTES
 router.post('/challenge/create', session.isUser, upload.single('contract'), sponsoring.challenge.create);
 router.post('/challenge/accept', session.isUser, sponsoring.challenge.accept);
 router.post('/challenge/reject', session.isUser, sponsoring.challenge.reject);
-//TODO wait for backend
 router.post('/challenge/delete', session.isUser, sponsoring.challenge.delete);
 
 module.exports = router;
