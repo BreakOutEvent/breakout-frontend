02739153aaef9c76c3e426978573a812810aa14d
Florian Schmidt <florian.schmidt.1994@icloud.com>
2017-01-25 22:34:03 +0100

    Test

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -6,6 +6,7 @@ if [ ! -z "$TRAVIS_TAG" ]; then
 
     echo "This will push to docker"
 
+    # Lulz
     # Decrypt and unzip secrets.zip.enc (contains config-stating.json + cert. for ssh)
     openssl aes-256-cbc -K $encrypted_c2aec72b3ee3_key -iv $encrypted_c2aec72b3ee3_iv -in secrets.zip.enc -out secrets.zip -d
     unzip secrets.zip
