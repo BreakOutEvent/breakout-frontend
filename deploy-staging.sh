75e0dc4144a892f54f56badb8fb78f4c0a791e35
Florian Schmidt <florian.schmidt.1994@icloud.com>
2017-01-25 22:01:20 +0100

    Add staging mechanism to travis

---
+++ deploy-staging.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "TRAVIS_TAG: $TRAVIS_TAG"
+
+if [ ! -z "$TRAVIS_TAG" ]; then
+
+    echo "This will push to docker"
+
+    # Decrypt and unzip secrets.zip.enc (contains config-stating.json + cert. for ssh)
+    openssl aes-256-cbc -K $encrypted_a6989f7c6f70_key -iv $encrypted_a6989f7c6f70_iv -in secrets.zip.enc -out secrets.zip -d
+    unzip secrets.zip
+
+    # Login at docker registry
+    docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS $DOCKER_URL
+
+    # Build docker image
+    docker build -f Dockerfile -t $DOCKER_REPO .
+
+    # Add tags and push to docker registry
+    docker tag $DOCKER_REPO $DOCKER_URL/$DOCKER_REPO:$TRAVIS_TAG
+    docker push $DOCKER_URL/$DOCKER_REPO:$TRAVIS_TAG
+    rm "/home/${USER}/.docker/config.json"
+
+    # If tag matches *stage deploy to staging
+    if [[ $TRAVIS_TAG == *stage ]]; then
+        echo "This will deploy to stage"
+        chmod 600 id_rsa_breakout_deploy
+        eval "$(ssh-agent -s)"
+        ssh-add id_rsa_breakout_deploy
+        ssh -o StrictHostKeyChecking=no $STAGE_LOGIN "docker-compose stop;"
+        scp -o StrictHostKeyChecking=no "docker-compose.yml" $STAGE_LOGIN
+        ssh -o StrictHostKeyChecking=no $STAGE_LOGIN "BACKEND_VERSION=latest BACKEND_PROFILE=staging FRONTEND_VERSION=$TRAVIS_TAG docker-compose pull"
+        ssh -o StrictHostKeyChecking=no $STAGE_LOGIN "BACKEND_VERSION=latest BACKEND_PROFILE=staging FRONTEND_VERSION=$TRAVIS_TAG docker-compose up -d"
+    else
+        echo "This will not deploy to stage, this is not with stage tag"
+    fi
+
+else
+    echo "This will not be pushed to docker, there is no tag"
+fi
