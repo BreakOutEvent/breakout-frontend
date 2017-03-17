e7f57acae95419aff586ddbbd9bdc4dfef6a81d6
Florian Schmidt <florian.schmidt.1994@icloud.com>
2017-03-17 11:15:52 +0100

    Disable counter until more performant implementation

--- services/websocket.js
+++ services/websocket.js
@@ -13,38 +13,38 @@ websocket.init = (io) => {
   var CronJob = require('cron').CronJob;
 
   //Event Infos
-  new CronJob('*/10 * * * * *', () => co(function*() {
-    var data = yield liveblog.getEventInfos();
-    io.sockets.emit('newEventInfos', data);
-  }), null, true);
+  // new CronJob('*/10 * * * * *', () => co(function*() {
+  //   var data = yield liveblog.getEventInfos();
+  //   io.sockets.emit('newEventInfos', data);
+  // }), null, true);
 
 
   //Postings
 
-  var oldData = [];
-  new CronJob('*/10 * * * * *', () => co(function*() {
-    var data = yield api.posting.getAllPostings(null, 0, 10);
-    var diff = [];
-    if (oldData.length === 0) {
-      //FIRST EXECUTION
-      oldData = data;
-    } else {
-      diff = compareData(oldData, data);
-      oldData = data;
-    }
-    if(diff.length > 0) {
-      var html = yield HBS.render('views/dynamic/liveblog/postings.handlebars',
-        {
-          layout: false,
-          postings: diff
-        }
-      );
-      io.sockets.emit('newPostings', {postings: html});
-    }
-
-  }).catch(err => {
-    // TODO: Logging
-  }), null, true);
+  // var oldData = [];
+  // new CronJob('*/10 * * * * *', () => co(function*() {
+  //   var data = yield api.posting.getAllPostings(null, 0, 10);
+  //   var diff = [];
+  //   if (oldData.length === 0) {
+  //     FIRST EXECUTION
+      // oldData = data;
+    // } else {
+    //   diff = compareData(oldData, data);
+    //   oldData = data;
+    // }
+    // if(diff.length > 0) {
+    //   var html = yield HBS.render('views/dynamic/liveblog/postings.handlebars',
+    //     {
+    //       layout: false,
+    //       postings: diff
+    //     }
+    //   );
+    //   io.sockets.emit('newPostings', {postings: html});
+    // }
+  //
+  // }).catch(err => {
+  //   TODO: Logging
+  // }), null, true);
 };
 
 function compareData(oldData, data) {
