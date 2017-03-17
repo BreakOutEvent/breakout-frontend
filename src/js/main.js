ffc3ce551f27d73d4cca7249f6d9de86fe7daeaa
Florian Schmidt <florian.schmidt.1994@icloud.com>
2017-03-17 10:35:51 +0100

    Set accessToken in store from traditional login as well

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,26 @@
 'use strict';
 require('./base.js');
 const $ = require('jquery');
+
+const BreakoutApi = require('./BreakoutApi');
+const store = require('store');
+
 var videoPlayer = require('./videoplayer.js');
 
-$(document).ready(function() {
+$(document).ready(function () {
   videoPlayer('#btnIntroVideo', '#modalIntroVideo');
+
+  if ($('#loginForm')) {
+    $('#loginForm').submit(function (e) {
+      e.preventDefault();
+      const username = this.username.value;
+      const pw = this.password.value;
+      const api = new BreakoutApi('http://localhost:8082', 'breakout_app', '123456789');
+      api.login(username, pw)
+        .then((data) => {
+          store.set('accessToken', data.access_token);
+        });
+      this.submit();
+    });
+  }
 });
