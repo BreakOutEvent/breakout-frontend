6ca7897ed30bd4afd12e59c6c70463dbd264c3a7
Florian Schmidt <florian.schmidt.1994@icloud.com>
2017-03-22 13:31:23 +0100

    Reimplement new registration flow in reactTest

--- src/js/reactTest.jsx
+++ src/js/reactTest.jsx
@@ -1,23 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './Components/Login.jsx';
+import RoleSelector from './Components/RoleSelector.jsx';
+import CreateOrJoinTeam from './Components/CreateOrJoinTeam.jsx';
+import BecomeParticipant from './Components/BecomeParticipant.jsx';
 import $ from 'jquery';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
-      visible: false
+      currentStep: null,
+      context: {}
+    };
+
+    this.steps = {
+      createOrJoinTeam: 'createOrJoinTeam',
+      login: 'login',
+      selectRole: 'roleSelect',
+      becomeParticipant: 'becomeParticipant',
+      done: ''
     };
   }
 
   componentWillMount() {
-    $('#bo-login-btn').click(() => this.setState({visible: true}));
+    $('#bo-login-btn').click(() => this.setState({
+      currentStep: this.steps.login
+    }));
   }
 
+  hide() {
+    this.setState({
+      currentStep: null
+    });
+    location.reload(true);
+  }
+
+  isActive(step) {
+    return step === this.state.currentStep;
+  }
+
+  transitionTo(step) {
+    this.setState({
+      currentStep: step
+    });
+  }
+
+
   render() {
-    return <Login visible={this.state.visible} onHide={() => this.setState({visible: false})}/>;
+    switch (this.state.currentStep) {
+      case null:
+        return null;
+
+      case this.steps.login:
+        return <Login onHide={this.hide.bind(this)}
+                      visible={this.isActive(this.steps.login)}
+                      next={this.transitionTo.bind(this)}
+                      steps={this.steps}
+                      context={this.state.context}/>;
+
+      case this.steps.createOrJoinTeam:
+        return <CreateOrJoinTeam onHide={this.hide.bind(this)}
+                                 visible={this.isActive(this.steps.createOrJoinTeam)}
+                                 next={this.transitionTo.bind(this)}
+                                 steps={this.steps}
+                                 context={this.state.context}/>;
+
+      case this.steps.selectRole:
+        return <RoleSelector onHide={this.hide.bind(this)}
+                             visible={this.isActive(this.steps.selectRole)}
+                             next={this.transitionTo.bind(this)}
+                             steps={this.steps}
+                             context={this.state.context}/>;
+
+      case this.steps.becomeParticipant:
+        return <BecomeParticipant onHide={this.hide.bind(this)}
+                                  visible={this.isActive(this.steps.becomeParticipant)}
+                                  next={this.transitionTo.bind(this)}
+                                  steps={this.steps}
+                                  context={this.state.context}/>;
+      case this.steps.done:
+        return null;
+
+      default:
+        return null;
+    }
   }
 }
 
