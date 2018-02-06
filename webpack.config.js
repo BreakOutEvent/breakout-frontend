7de351b8a4e809f7412d2cc002d31294d1bb54b9
Florian Schmidt <florian.schmidt.1994@icloud.com>
2018-02-06 22:41:05 +0100

    Use webpack for css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 'use strict';
 const webpack = require('webpack');
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractLess = new ExtractTextPlugin({
+  filename: '[name].css'
+});
 
 module.exports = {
   entry: {
@@ -15,6 +20,7 @@ module.exports = {
     team: ['./src/client/js/team.js'],
     liveblog: ['./src/client/js/liveblog.js'],
     landingpage: ['./src/client/js/landingpage.js'],
+    styles: ['./src/less/styles.less']
   },
   output: {
     path: path.resolve(__dirname, 'public/js'),
@@ -38,7 +44,25 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.(less)$/,
+        include: path.resolve(__dirname, 'src/less/'),
+        use: extractLess.extract({
+          use: [{
+            loader: 'css-loader'
+          }, {
+            loader: 'less-loader'
+          }]
+        })
+      }, {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
       }
     ]
-  }
+  },
+  plugins: [extractLess]
 };
