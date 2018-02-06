ad9dac6b27d3bf7a15d2511762c600e1b5d0f8ec
Florian Schmidt <florian.schmidt.1994@icloud.com>
2018-02-06 23:38:30 +0100

    Remove dead code in schemas folder

--- schemas/languagetovalue.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports =  new mongoose.Schema({
-  language: { type: mongoose.Schema.Types.Lang, required: true },
-  value: { type: String, required: true },
-});
