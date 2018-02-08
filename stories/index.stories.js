54f1ab693f6e9a70fa81d5e874b513ef81ab6347
Florian Schmidt <florian.schmidt.1994@icloud.com>
2018-02-08 19:34:30 +0100

    Fix linting errors

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -6,17 +6,18 @@ import {linkTo} from '@storybook/addon-links';
 
 import {Button, Welcome} from '@storybook/react/demo';
 import InvoiceTable from '../src/client/react/Components/Admin/InvoiceTable';
-import {MuiThemeProvider} from "material-ui";
+import {MuiThemeProvider} from 'material-ui';
 
 // Place useful samples here, can't do that currently due to privacy reasons
 import InvoiceSampleData from './invoiceSampleData';
 import EventSampleData from './eventSampleData';
 
-import SortableInvoiceTable from "../src/client/react/Components/Admin/SortableInvoiceTable";
+import SortableInvoiceTable from '../src/client/react/Components/Admin/SortableInvoiceTable';
 
 
 storiesOf('Invoice Table', module)
   .add('default', () => <MuiThemeProvider><InvoiceTable data={InvoiceSampleData}/></MuiThemeProvider>)
   .add('search and sortable', () => <MuiThemeProvider><SortableInvoiceTable events={EventSampleData}
                                                                             data={InvoiceSampleData}
-                                                                            eventSelected={() => {}}/></MuiThemeProvider>);
+                                                                            eventSelected={() => {
+                                                                            }}/></MuiThemeProvider>);
