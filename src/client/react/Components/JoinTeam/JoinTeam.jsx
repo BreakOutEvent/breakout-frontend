2ae972b50c18df990eb699cddfb1d51d6cbd9725
dominikglandorf <do.gl@gmx.de>
2018-04-01 23:41:37 +0200

    JoinTeam: fix props type check

--- src/client/react/Components/JoinTeam/JoinTeam.jsx
+++ src/client/react/Components/JoinTeam/JoinTeam.jsx
@@ -78,5 +78,5 @@ JoinTeam.propTypes = {
   api: React.PropTypes.object.isRequired,
   i18next: React.PropTypes.object.isRequired,
   history: React.PropTypes.object.isRequired,
-  invitations: React.PropTypes.object.isRequired
+  invitations: React.PropTypes.array.isRequired
 };
