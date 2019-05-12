444c031f93ec30d91d2069f0205692dade70b3dc
Mathias Quintero <me@quintero.io>
2019-05-12 12:05:32 +0100

    Adding jquery to eslint env

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@
 module.exports = {
     "env": {
         "es6": true,
-        "node": true
+        "node": true,
+        "jquery": true
     },
     "extends": "eslint:recommended",
     "rules": {
