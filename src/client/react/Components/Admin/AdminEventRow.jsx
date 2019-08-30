4d529135a2cde77c5449cac31b80e696c6a5723c
Mathias Quintero <me@quintero.io>
2019-08-31 00:14:04 +0200

    Adding new event modal

--- src/client/react/Components/Admin/AdminEventRow.jsx
+++ src/client/react/Components/Admin/AdminEventRow.jsx
@@ -1,164 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
-import {
-  Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControlLabel, TextField, Switch, Grid, MuiThemeProvider
-} from '@material-ui/core';
-
-import {DateFormatInput, TimeFormatInput} from 'material-ui-next-pickers';
-
-import LocationPicker from 'react-location-picker';
-
-const sampleEvent = {
-  title: 'Sample Event',
-  city: 'Munich',
-  date: new Date(),
-  duration: 36,
-  latitude: 48.150676,
-  longitude: 11.580984,
-  isCurrent: false,
-  isOpenForRegistration: false,
-  allowNewSponsoring: false,
-};
-
-function Modal(props) {
-  const [event, setEvent] = useState(props.event || sampleEvent);
-  const [date, setDate] = useState(new Date(event.date * 1000));
-  const [address, setAddress] = useState(null);
-  const [location, setLocation] = useState({lat: props.event.startingLocation.latitude, lng: props.event.startingLocation.longitude});
-
-  useEffect(() => {
-    setEvent({ ...event, date: date.getTime() / 1000 });
-  }, [date]);
-
-  useEffect(() => {
-    setEvent({ ...event, startingLocation: { latitude: location.lat, longitude: location.lng } });
-  }, [location]);
-
-  const save = async (e) => {
-    if (e) e.preventDefault();
-    props.onChange(event);
-    props.onClose();
-  };
-
-  return (
-    <Dialog
-      open={props.open}
-      fullScreen={false}
-      onClose={props.onClose}>
-      <form onSubmit={save}>
-        <DialogTitle id="event-details">Event Details</DialogTitle>
-        <DialogContent>
-          <Grid container spacing={8} alignItems="stretch">
-            <Grid item xs={12}>
-              <TextField
-                autoFocus
-                label="Title"
-                value={event.title}
-                onChange={change => setEvent({ ...event, title: change.target.value })}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                label="City"
-                value={event.city}
-                onChange={change => setEvent({ ...event, city: change.target.value })}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                label="Brand"
-                value={event.brand}
-                onChange={change => setEvent({ ...event, brand: change.target.value })}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                label="Fee"
-                type="number"
-                value={event.teamFee}
-                onChange={change => setEvent({ ...event, teamFee: change.target.value })}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              {address !== null && <h5>{address}</h5>}
-              <LocationPicker
-                defaultPosition={location}
-                onChange={({address, position}) => {
-                  setAddress(address);
-                  setLocation(position);
-                }}
-                radius={-1}
-                zoom={15}
-                containerElement={<div style={{ height: '100%' }}/>}
-                mapElement={<div style={{ height: '400px' }}/>}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <DateFormatInput
-                value={date}
-                onChange={setDate}
-                dateFormat="dd.MM.yyyy"
-                InputLabelProps={{
-                  shrink: false,
-                }}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <div style={{}}></div>
-              <TimeFormatInput
-                value={date}
-                onChange={setDate}
-                InputLabelProps={{
-                  shrink: false,
-                }}
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <FormControlLabel
-                control={
-                  <Switch
-                    checked={event.isCurrent}
-                    onChange={(change, checked) => setEvent({ ...event, isCurrent: checked })}
-                  />  
-                }
-                label="Is Current"
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <FormControlLabel
-                control={
-                  <Switch
-                    checked={event.openForRegistration}
-                    onChange={(change, checked) => setEvent({ ...event, openForRegistration: checked })}
-                  />  
-                }
-                label="Is Open For Registraion"
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <FormControlLabel
-                control={
-                  <Switch
-                    checked={event.allowNewSponsoring}
-                    onChange={(change, checked) => setEvent({ ...event, allowNewSponsoring: checked })}
-                  />
-                }
-                label="Allow new Sponsorings"
-              />
-            </Grid>
-          </Grid>
-        </DialogContent>
-        <DialogActions style={{ justifyContent: 'flex-end' }}>
-          <Button onClick={props.onClose} color="primary">
-            Cancel
-          </Button>
-          <Button type="submit" color="primary">
-            Save
-          </Button>
-        </DialogActions>
-      </form>
-    </Dialog>
-  );
-}
+import { Button, Switch } from '@material-ui/core';
+import EditEventModal from './EditEventModal.jsx';
 
 export default function AdminEventRow(props) {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
@@ -191,7 +33,7 @@ export default function AdminEventRow(props) {
       </td>
       <td>
         {event.duration} hours
-            </td>
+      </td>
       <td>
         <Switch disabled={!props.canEdit} checked={event.isCurrent} onChange={(event, checked) => toggle({ isCurrent: checked })} />
       </td>
@@ -205,7 +47,7 @@ export default function AdminEventRow(props) {
         <Button disabled={!props.canEdit} color="secondary" onClick={() => setIsDialogOpen(true)}>Edit</Button>
       </td>
 
-      <Modal open={isDialogOpen} event={event} onChange={event => setEvent(event)} onClose={() => setIsDialogOpen(false)} />
+      <EditEventModal open={isDialogOpen} event={event} onChange={event => setEvent(event)} onClose={() => setIsDialogOpen(false)} />
     </tr>
   );
 }
--- src/client/react/Components/Admin/AdminEventsOverview.jsx
+++ src/client/react/Components/Admin/AdminEventsOverview.jsx
@@ -1,10 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import AdminEventRow from './AdminEventRow.jsx';
+import EditEventModal from './EditEventModal.jsx';
+
+import {Button} from '@material-ui/core';
+
+const sampleEvent = {
+  title: 'Sample Event',
+  city: 'Munich',
+  brand: `BreakOut ${(new Date()).getFullYear()}`,
+  teamFee: 60.0,
+  date: (new Date()).getTime() / 1000,
+  duration: 36,
+  startingLocation: {
+    latitude: 48.150676,
+    longitude: 11.580984
+  },
+  isCurrent: false,
+  isOpenForRegistration: false,
+  allowNewSponsoring: false,
+};
 
 export default function AdminEventsOverview(props) {
   const [isLoading, setIsLoading] = useState(false);
   const [events, setEvents] = useState([]);
   const [canEdit, setCanEdit] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const init = () => {
     setIsLoading(true);
@@ -17,34 +37,47 @@ export default function AdminEventsOverview(props) {
     });
   };
 
+  const addEvent = async (event) => {
+    const newEvent = await props.api.createEvent(event);
+    setEvents([newEvent, ...events]);
+  };
+
   useEffect(() => {
     init();
   }, []);
 
   return (
     <div> {isLoading && <h1>Loading...</h1>}{!isLoading &&
-      <div style={{ display: 'flex' }}>
+      <div>
         <div className="row">
           <div className="col-xs-12">
-            <table className="table table-striped">
-              <thead>
-                <tr>
-                  <th>Event</th>
-                  <th>City</th>
-                  <th>Date</th>
-                  <th>Duration</th>
-                  <th>Is Current</th>
-                  <th>Is Open for Registration</th>
-                  <th>Allows new Sponsorings</th>
-                  <th>Edit</th>
-                </tr>
-              </thead>
-              <tbody id="list">
-                {events.map(event => <AdminEventRow key={event.id} api={props.api} canEdit={canEdit} event={event}/>)}
-              </tbody>
-            </table>
+            <Button disabled={!canEdit} color="secondary" onClick={() => setIsDialogOpen(true)}>New Event</Button>
           </div>
         </div>
+        <div style={{ display: 'flex' }}>
+          <div className="row">
+            <div className="col-xs-12">
+              <table className="table table-striped">
+                <thead>
+                  <tr>
+                    <th>Event</th>
+                    <th>City</th>
+                    <th>Date</th>
+                    <th>Duration</th>
+                    <th>Is Current</th>
+                    <th>Is Open for Registration</th>
+                    <th>Allows new Sponsorings</th>
+                    <th>Edit</th>
+                  </tr>
+                </thead>
+                <tbody id="list">
+                  {events.map(event => <AdminEventRow key={event.id} api={props.api} canEdit={canEdit} event={event}/>)}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <EditEventModal open={isDialogOpen} event={sampleEvent} onChange={event => addEvent(event)} onClose={() => setIsDialogOpen(false)} />
       </div>
     }
     </div>
