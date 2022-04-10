bdf244c98c0e6ebb7d88e5fe0c8539be03a492ed
Sebastian Schubotz <sebastian.schubotz@break-out.org>
2022-04-10 16:15:07 +0200

    Remove instafeed as it's been broken for a while now

--- src/client/js/landingpage.js
+++ src/client/js/landingpage.js
@@ -1,18 +1,4 @@
-const Instafeed = require('instafeed.js');
 const $ = require('jquery');
-
-var feed = new Instafeed({
-  get: 'user',
-  userId: '3120245646',
-  clientId: '5d325f2ba927465d9c3933be01ee870c',
-  accessToken: '3120245646.1677ed0.442f6331662045c0a14c9802e799ccf6',
-  resolution: 'standard_resolution',
-  template: '<div class="instaimage" style="background-image: url(\'\{{image}}\');"><a href="https://www.instagram.com/breakout_ev/"><span class="instaCaption">\{{caption}}</span><span class="instHover"></span></div></a>',
-  limit: 20
-});
-
-
-feed.run();
 
 // Register listeners for video section play button and link
 $(function () {
