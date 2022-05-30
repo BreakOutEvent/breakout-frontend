5cf432caa957fd628d63a086fd67f96f2e511e57
Simon Koller <simon.koller@abat.de>
2022-05-30 12:11:43 +0200

    feature/move-team-profile | addd visibility checks for deleting team profile

--- src/client/react/Components/TeamProfile/ListOfSettings.jsx
+++ src/client/react/Components/TeamProfile/ListOfSettings.jsx
@@ -6,32 +6,38 @@ import PropTypes from 'prop-types';
 class ListOfSettings extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {error: null};
+    this.state = { error: null };
   }
 
 
-  deleteTeamProfile(teamId)  {
+  deleteTeamProfile(teamId) {
     this.props.api.deleteTeam(this.props.teamId)
       .then(data => {
         window.location.href = routes.teamDeletionSuccess;
       })
       .catch(error => {
 
-        this.setState({error});
+        this.setState({ error });
       });
   }
 
   render() {
     const i18next = this.props.i18next;
+
+    if (!window.boUserData.me || !window.boUserData.me.participant || window.boUserData.me.participant.teamId !== this.props.teamId) {
+      return null;
+    }
+
     if (this.state.error) {
       return <div className="alert alert-warning">Something went wrong when loading settings.</div>;
     }
 
     return <div>
-         <Button id='deleteTeamProfile' color="secondary" size="small"
+      <h3>{i18next.t('TEAM-DETAIL.SETTINGS')}</h3>
+      <Button id='deleteTeamProfile' color="secondary" size="small"
         onClick={() => this.deleteTeamProfile(this.props.teamId)}>
-            {i18next.t('client.team_settings.delete_team')}
-          </Button>
+        {i18next.t('client.team_settings.delete_team')}
+      </Button>
     </div>;
   }
 }
